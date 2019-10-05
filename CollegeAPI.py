55e9395e2e4d11b7e50de203eb17f9f21221e4b5
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-10-05 15:07:10 -0400

    Migrated to Postgres fro Heroku

--- CollegeAPI.py
+++ CollegeAPI.py
@@ -6,7 +6,7 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'CollegeAPI.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
