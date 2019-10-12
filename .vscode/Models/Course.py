b8dfcf9a8745dc7f9b523632ee7513ccc4b39278
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-10-12 18:27:43 -0400

    Changed Course  model attributes to english

--- .vscode/Models/Course.py
+++ .vscode/Models/Course.py
@@ -4,24 +4,24 @@ from CollegeAPI import app
 
 class Materia(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    nombre = db.Column(db.String(50))
-    codigo = db.Column(db.String(6))
-    seccion = db.Column(db.Integer)
-    aula = db.Column(db.String(10))
-    horarioDias = db.Column(db.String(30))
-    horarioHoras = db.Column(db.String(15))
-    profesor = db.Column(db.String(50))
+    CourseName = db.Column(db.String(50))
+    CourseID = db.Column(db.String(6))
+    CourseSection = db.Column(db.Integer)
+    CourseRoom = db.Column(db.String(10))
+    ScheduleDays = db.Column(db.String(30))
+    ScheduleHours = db.Column(db.String(15))
+    CourseTeacher = db.Column(db.String(50))
 
-    def __init__(self,nombre,codigo,seccion,aula,horarioDias,horarioHoras,profesor):
+    def __init__(self,CourseName,CourseID,CourseSection,CourseRoom,ScheduleDays,ScheduleHours,CourseTeacher):
 
-        self.nombre = nombre
-        self.codigo = codigo
-        self.seccion = seccion
-        self.aula = aula
-        self.horarioDias = horarioDias
-        self.horarioHoras = horarioHoras
-        self.profesor = profesor
+        self.CourseName = CourseName
+        self.CourseID = CourseID
+        self.CourseSection = CourseSection
+        self.CourseRoom = CourseRoom
+        self.ScheduleDays = ScheduleDays
+        self.ScheduleHours = ScheduleHours
+        self.CourseTeacher = CourseTeacher
 
 class MateriaSchema(ma.Schema):
     class Meta:
-        fields = ('nombre','codigo','seccion','aula','horarioDias','horarioHoras','profesor')
+        fields = ('CourseName','CourseID','CourseSection','CourseRoom','ScheduleDays','ScheduleHours','CourseTeacher')
