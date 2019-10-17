ef7d399e6dca9ca075f86e580b0fbb78ef34a4d2
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-10-17 19:41:20 -0400

    Conditioned the database for developing environment

--- CollegeAPI.py
+++ CollegeAPI.py
@@ -5,8 +5,8 @@ from sqlalchemy import or_,and_
 import os
 
 app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_BLACK_URL'] 
+basedir = os.path.abspath(os.path.dirname(__file__)) #This can be eliminated in production
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ os.path.join(basedir,'NewBeeCore.sqlite') #os.environ['HEROKU_POSTGRESQL_BLACK_URL'] #Production DB
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
