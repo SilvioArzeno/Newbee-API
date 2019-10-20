719677483c12705db0b3ffaf5f3a3efe21bb41fa
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-10-20 15:58:38 -0400

    Registered all module blueprints

--- CollegeAPI/__init__.py
+++ CollegeAPI/__init__.py
@@ -1,13 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-import os
 
 app = Flask(__name__)
 app.config.from_pyfile('../config.py')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+#Blueprints registration
 from CollegeAPI.StudentModule.controllers import app as StudentModule
+from CollegeAPI.CourseModule.controllers import app as CourseModule
+from CollegeAPI.DirectoryModule.controllers import app as DirectoryModule
+from CollegeAPI.ScheduleModule.controllers import app as ScheduleModule
+
 app.register_blueprint(StudentModule)
+app.register_blueprint(CourseModule)
+app.register_blueprint(DirectoryModule)
+app.register_blueprint(ScheduleModule)
 
