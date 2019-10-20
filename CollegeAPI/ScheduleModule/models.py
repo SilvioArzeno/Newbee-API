80bf13586fe100082784b007e2f6e68b10ffc771
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-10-20 15:42:24 -0400

    Added Schedule module and finished Models

--- CollegeAPI/ScheduleModule/models.py
+++ CollegeAPI/ScheduleModule/models.py
@@ -1,15 +1,15 @@
-from CollegeAPI import ma,db,app
+from CollegeAPI import ma,db
 
 class Schedule(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     StudentID = db.Column(db.String(7))
-    MateriaID = db.Column(db.String(10))
+    CourseID = db.Column(db.String(10))
 
-    def __init__(self,StudentID,MateriaID):
+    def __init__(self,StudentID,CourseID):
 
         self.StudentID = StudentID
-        self.MateriaID = MateriaID
+        self.CourseID = CourseID
 
 class ScheduleSchema(ma.Schema):
     class Meta:
-        fields = ('StudentID','MateriaID')
+        fields = ('StudentID','CourseID')
