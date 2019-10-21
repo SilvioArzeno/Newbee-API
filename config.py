6a2ce3a1d0660e356832ce33d1d0e85c9df3e390
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-10-21 19:23:01 -0400

    API ready for Production once more

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 
-basedir = os.path.abspath(os.path.dirname(__file__)) #This can be eliminated in production
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///'+ os.path.join(basedir,'NewBeeCore.sqlite') #os.environ['HEROKU_POSTGRESQL_BLACK_URL'] #Production DB
+SQLALCHEMY_DATABASE_URI = os.environ['HEROKU_POSTGRESQL_BLACK_URL']
